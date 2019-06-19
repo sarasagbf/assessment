a1a16d061e46607a8524d6d935902cbc47a2bd17
sarasagbf <40536261+sarasagbf@users.noreply.github.com>
2019-06-19 20:59:36 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -85,7 +85,7 @@
 
       // 文字のコード番号の合計を回答の数で割って添字の数値を求める
       const index = sumOfcharCode % answers.length;
-      let result = answers[16];
+      let result = answers[index];
 
       result = result.replace(/{userName}/g, userName);
       return result;
